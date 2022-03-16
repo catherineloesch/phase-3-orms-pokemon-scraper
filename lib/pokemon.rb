b3ba38af35007c8393530c94c9750c0df2aeaaf4
Catherine Loesch <catherine.loesch@protonmail.com>
2022-03-16 05:05:36 +0000

    lab completed

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,21 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+        db.execute(sql, name, type)
+    end
+
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        search = db.execute(sql, id).first
+        self.new(id: search[0], name: search[1], type: search[2], db: db)
+    end
 end
